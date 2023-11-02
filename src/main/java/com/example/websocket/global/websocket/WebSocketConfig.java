6b42ab94d5df09ded743d101542829c369868b0d
seonghye0n <>
2023-11-02 17:21:02 +0900

    feat : 채팅 접속 만료 시간 설정
    - 채팅방 접속 후, 10분 간 아무 입력없으면 연결 해제

--- src/main/java/com/example/websocket/global/websocket/WebSocketConfig.java
+++ src/main/java/com/example/websocket/global/websocket/WebSocketConfig.java
@@ -1,11 +1,13 @@
 package com.example.websocket.global.websocket;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -19,5 +21,13 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(webSocketHandler, "/chat/*").setAllowedOrigins("*")
             .addInterceptors(customHandShakeInterceptor);
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxSessionIdleTimeout(600000L);
+
+        return container;
     }
 }
