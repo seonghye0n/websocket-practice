6c4433117efca2a7fbb04ee209ed40fb38fb545c
seonghye0n <>
2023-11-02 17:30:49 +0900

    feat : Member Entity 생성

---
+++ src/main/java/com/example/websocket/domain/Member.java
@@ -0,0 +1,39 @@
+package com.example.websocket.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 15)
+    private String nickname;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
--- src/main/resources/h2/data.sql
+++ src/main/resources/h2/data.sql
@@ -1,5 +1,13 @@
+-------------- GAME --------------
 INSERT INTO game(home, away, status, created_at)
 VALUES ('한화', 'NC', 'READY', CURRENT_TIMESTAMP);
 
 INSERT INTO game(home, away, status, created_at)
 VALUES ('삼성', '두산', 'PROCEEDING', CURRENT_TIMESTAMP);
+
+-------------- MEMBER --------------
+INSERT INTO member(username, password, nickname, created_at)
+VALUES ('seonghye0n', 'test11', 'seonghye0n', CURRENT_TIMESTAMP);
+
+INSERT INTO member(username, password, nickname, created_at)
+VALUES ('test11', 'test11', '테스트', CURRENT_TIMESTAMP);
