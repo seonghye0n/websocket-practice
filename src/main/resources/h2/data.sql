1ae443502e29f188e127a4828924930cb8044483
seonghye0n <>
2023-11-06 15:52:15 +0900

    chore : 더미데이터 패스워드 암호화
    - 회원 더미데이터 패스워드 암호화 적용

--- src/main/resources/h2/data.sql
+++ src/main/resources/h2/data.sql
@@ -7,7 +7,9 @@ VALUES ('삼성', '두산', 'PROCEEDING', CURRENT_TIMESTAMP);
 
 -------------- MEMBER --------------
 INSERT INTO member(username, password, nickname, created_at)
-VALUES ('seonghye0n', 'test11', 'seonghye0n', CURRENT_TIMESTAMP);
+VALUES ('seonghye0n', '$2a$12$y/ookJ2cQ2BDplpauu7h1uIY93qAhOxxEfoqMgl18jAgPdOr0Kp0u', 'seonghye0n',
+        CURRENT_TIMESTAMP);
 
 INSERT INTO member(username, password, nickname, created_at)
-VALUES ('test11', 'test11', '테스트', CURRENT_TIMESTAMP);
+VALUES ('test11', '$2a$12$y/ookJ2cQ2BDplpauu7h1uIY93qAhOxxEfoqMgl18jAgPdOr0Kp0u', '테스트',
+        CURRENT_TIMESTAMP);
